8d9bb83d90818020c9765e70f0961df2a04475ee
Noah Lavayen <noah.lavayen2@gmail.com>
2025-10-02 15:38:42 -0400

    fix 3d room model

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -1,7 +1,7 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useMediaQuery } from "react-responsive";
-import Room from "./Room.jsx";
+import { Room } from "./Room";
 
 const HeroExperience = () => {
   const isTablet = useMediaQuery({ query: "(max-width: 724px)" });
