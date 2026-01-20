1451749b8fdf7286e2a9096c254b81734b53bfcd
Noah Lavayen <noah.lavayen2@gmail.com>
2026-01-20 13:56:23 -0500

    implement Experience section

--- src/sections/FeatureCards.jsx
+++ src/sections/FeatureCards.jsx
@@ -12,7 +12,7 @@ const FeatureCards = () => {
             <div className="size-14 flex items-center justify-center rounded-full">
               <img src={imgPath} alt={title} />
             </div>
-            <h3 className="text-white text-2xl font-semibold">{title}</h3>\
+            <h3 className="text-white text-2xl font-semibold">{title}</h3>
             <p className="text-white-50 text-lg">{desc}</p>
           </div>
         ))}
